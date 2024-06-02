1a4539b8e427ad8fc9a8311d76103dfb55a27165
Robert Soghbatyan <robert.sogbatyan@gmail.com>
2024-06-02 13:33:00 +0400

    Added transition when switching between the pages

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
-import {Outlet} from 'react-router';
-import {StyledHeaderWrapper, StyledLayout, StyledMain} from './styled';
+import React, {createRef, useEffect, useRef} from 'react';
+import {useLocation, useOutlet} from 'react-router';
+import {CSSTransition, SwitchTransition} from 'react-transition-group';
+import {StyledHeaderWrapper, StyledLayout, StyledMain, StyledPage} from './styled';
 
 const Layout: React.FC = () => {
+  const location = useLocation();
+  const currentOutlet = useOutlet();
+
+  const nodeRefMap = useRef<Map<string, React.RefObject<HTMLDivElement>>>(new Map());
+  const nodeRef = nodeRefMap?.current.get(location.pathname);
+
+  useEffect(() => {
+    if (!nodeRefMap.current.get(location.pathname)) {
+      nodeRefMap.current.set(location.pathname, createRef());
+    }
+  }, [location.pathname]);
+
   return (
     <StyledLayout>
       <StyledHeaderWrapper />
 
       <StyledMain>
-        <Outlet />
+        <SwitchTransition>
+          <CSSTransition
+            key={location.pathname}
+            nodeRef={nodeRef}
+            timeout={150}
+            classNames={'page'}
+            unmountOnExit
+          >
+            {() => <StyledPage ref={nodeRef}>{currentOutlet}</StyledPage>}
+          </CSSTransition>
+        </SwitchTransition>
       </StyledMain>
     </StyledLayout>
   );
--- src/components/layout/styled.tsx
+++ src/components/layout/styled.tsx
@@ -15,7 +15,38 @@ const StyledHeaderWrapper = styled(Header)`
 const StyledMain = styled.main`
   flex-shrink: 1;
   flex-grow: 1;
+
+  .page-enter {
+    opacity: 0;
+    transform: scale(1.1);
+  }
+
+  .page-enter-active {
+    opacity: 1;
+    transform: scale(1);
+    transition:
+      opacity 150ms,
+      transform 150ms;
+  }
+
+  .page-exit {
+    opacity: 1;
+    transform: scale(1);
+  }
+
+  .page-exit-active {
+    opacity: 0;
+    transform: scale(0.9);
+    transition:
+      opacity 150ms,
+      transform 150ms;
+  }
+`;
+
+const StyledPage = styled.div`
+  width: 100%;
+  height: 100%;
   overflow: auto;
 `;
 
-export {StyledHeaderWrapper, StyledLayout, StyledMain};
+export {StyledHeaderWrapper, StyledLayout, StyledMain, StyledPage};
